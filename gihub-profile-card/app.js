0dbeb8d600765de7310186576910cd45c3121858
SoleneLivran <s.livran@gmail.com>
2020-08-01 02:22:49 +0200

    Github profile cards

---
+++ gihub-profile-card/app.js
@@ -0,0 +1,31 @@
+// let GithubUserCardComponent = {
+Vue.component('github-user-card', {
+    template : '#github-user-card-template',
+    props: {
+        username: {
+          type: String,
+          required: true
+        }
+    },
+    data () {
+        return {
+          user: {}
+        }
+    },
+    created() {
+        axios.get(`https://api.github.com/users/${this.username}`)
+          .then((response) => {
+            this.user = response.data
+          })
+      }
+  })
+
+new Vue({
+    el: '#app',
+    // components: {
+    //   'github-user-card': GithubUserCardComponent
+    // },
+    data: {
+        usernames: ['matts2cant', 'SoleneLivran','hootlex', 'rahaug', 'sdras', 'akryum']
+    }
+})
